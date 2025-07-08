4fc5c5e24322dc97e7e4e348d8480bdf6d476e45
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2025-07-08 15:15:23 +0900

    feat: Calendar 컴포넌트 작성

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import {
+	Dimensions,
 	FlatList,
 	StyleSheet,
 	Text,
@@ -9,26 +10,94 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
 
+const deviceWidth = Dimensions.get("window").width;
+
+const rednerItem = ({
+	item,
+	isCurrentMonth,
+	isSelected,
+	onPress,
+}: {
+	item: dayjs.Dayjs | null;
+	isCurrentMonth: boolean;
+	isSelected: boolean;
+	onPress: () => void;
+}) => {
+	const textColor = isCurrentMonth ? "black" : "lightgray";
+	return (
+		<TouchableOpacity
+			style={{
+				width: deviceWidth / 7,
+				height: deviceWidth / 7,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+			onPress={onPress}
+		>
+			<View
+				style={{
+					width: 30,
+					height: 30,
+					justifyContent: "center",
+					alignItems: "center",
+					borderRadius: 15,
+					borderWidth: 1,
+					borderColor: isSelected ? "blue" : "transparent",
+				}}
+			>
+				<Text
+					style={{
+						color: isCurrentMonth ? textColor : "lightgray",
+					}}
+				>
+					{item?.format("D")}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
+};
+
 export const Calendar = () => {
-	const [currentDate, setCurrentDate] = useState<Date>(dayjs().toDate());
+	const [selectedMonth, setSelectedMonth] = useState<dayjs.Dayjs>(dayjs());
+	const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs>(selectedMonth);
+
+	const columns = useMemo(() => {
+		const startOfMonth = selectedMonth.startOf("month");
+		const endOfMonth = selectedMonth.endOf("month");
+
+		const columns: (dayjs.Dayjs | null)[] = [];
+
+		for (let i = 1; i <= startOfMonth.get("day"); i++) {
+			columns.push(dayjs(startOfMonth).subtract(i, "day"));
+		}
+
+		for (let i = 0; i < endOfMonth.get("date"); i++) {
+			columns.push(dayjs(startOfMonth).add(i, "day"));
+		}
+
+		const diff = 6 - endOfMonth.get("day");
+		for (let i = 1; i <= diff; i++) {
+			columns.push(dayjs(endOfMonth).add(i, "day"));
+		}
+
+		return columns;
+	}, [selectedMonth]);
 
 	const handlePreviousMonth = () => {
-		setCurrentDate(dayjs(currentDate).subtract(1, "month").toDate());
+		setSelectedMonth(dayjs(selectedMonth).subtract(1, "month"));
 	};
 
 	const handleNextMonth = () => {
-		setCurrentDate(dayjs(currentDate).add(1, "month").toDate());
+		setSelectedMonth(dayjs(selectedMonth).add(1, "month"));
 	};
 
-	const daysInMonth = dayjs(currentDate).daysInMonth();
-
 	return (
-		<View>
+		<View style={{ flex: 1 }}>
 			<View style={styles.header}>
 				<TouchableOpacity onPress={handlePreviousMonth}>
 					<Icon name="left" size={24} color="black" />
 				</TouchableOpacity>
-				<Text>{dayjs(currentDate).format("MMMM YYYY")}</Text>
+				<Text>{dayjs(selectedMonth).format("MMMM YYYY")}</Text>
 				<TouchableOpacity onPress={handleNextMonth}>
 					<Icon name="right" size={24} color="black" />
 				</TouchableOpacity>
@@ -42,10 +111,25 @@ export const Calendar = () => {
 				<Text style={{ color: "gray" }}>Fri</Text>
 				<Text style={{ color: "blue" }}>Sat</Text>
 			</View>
-			<View style={styles.body}>
+			<View>
 				<FlatList
-					data={Array.from({ length: daysInMonth })}
-					renderItem={({ index }) => <Text>{index + 1}</Text>}
+					data={columns}
+					numColumns={7}
+					renderItem={({ item }) =>
+						rednerItem({
+							item,
+							isCurrentMonth: !!item?.isSame(selectedMonth, "month"),
+							isSelected:
+								item?.format("YYYY-MM-DD") ===
+									selectedDate.format("YYYY-MM-DD") &&
+								!!item?.isSame(selectedMonth, "month"),
+							onPress: () => {
+								if (item) {
+									setSelectedDate(item);
+								}
+							},
+						})
+					}
 				/>
 			</View>
 		</View>
@@ -64,8 +148,5 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between",
 		alignItems: "center",
 		padding: 16,
-	},
-	body: {
-		flex: 1,
 	},
 });
